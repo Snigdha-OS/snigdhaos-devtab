6abe490e722fb14faeec824aa2604d22ea91dd8d
eshanized <m.eshanized@gmail.com>
2024-12-24 19:22:48 +0530

    🐛 fix(bookmark): module not working

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,10 @@
 export * from './settings';
 export * from './devto';
+
+// Local type definition in Bookmarks component
+export interface Bookmark {
+    id: string;
+    title: string;
+    url: string;
+  }
+  
