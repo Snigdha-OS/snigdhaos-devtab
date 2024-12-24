a27b8d1aa0df9720ef853e4e2f2295f2d6bd2051
eshanized <m.eshanized@gmail.com>
2024-12-24 20:22:19 +0530

    🚀 feat(mods): some major modification

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/snigdhaos-devtab/', // Make sure this matches your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
